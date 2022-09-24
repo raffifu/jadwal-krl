56e2c47474bce3a067a19c613991a6e58efb978b
Raffi Utama <raffifebriandikau@gmail.com>
2022-09-24 20:11:47 +0700

    Add moment and change DateUtils

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -1,32 +1,17 @@
+import moment from 'moment';
 import { TimeRange } from '../model/api';
 
 export default class DateUtils {
-  private dateNow: Date;
-
-  private endDate: Date;
-
-  private options: Intl.DateTimeFormatOptions = {
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: false,
-  };
+  private moment: moment.Moment;
 
   constructor() {
-    const unixTime = Date.now();
-    this.dateNow = new Date(unixTime);
-    this.endDate = new Date(unixTime);
+    this.moment = moment();
   }
 
   public getTimeRange(diff: number) : TimeRange {
-    this.addHours(diff);
-
     return {
-      start: this.dateNow.toLocaleString('en-US', this.options),
-      end: this.endDate.toLocaleString('en-US', this.options),
+      start: this.moment.format('HH:mm'),
+      end: this.moment.add(diff, 'hours').format('HH:mm'),
     };
-  }
-
-  private addHours(diff: number) {
-    this.endDate.setHours(this.endDate.getHours() + diff);
   }
 }
